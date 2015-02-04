1a4eb616f63e76c8abcb9f60386b8a95901d2eea
lijunjie <lijunjieone@gmail.com>
2015-02-04 17:39:08 +0800

    修改成可以运行的代码

--- jni/hello-jni.c
+++ jni/hello-jni.c
@@ -2,11 +2,25 @@
 #include <jni.h>
 #include <unistd.h>
 
+static jstring func1(JNIEnv * env) {
+    char* arg5 = "hello jni5";
+
+//env=0;
+	return (*env)->NewStringUTF(env,arg5);
+}
 
 JNIEXPORT jstring JNICALL Java_com_mx_jnilearn_MainActivity_getTextFromJni
   (JNIEnv * env, jobject thiz){
     char* arg = "hello jni";
 
-    return (*env)->NewStringUTF(env,arg );
+    char* arg2 = "hello jni2";
+
+    char* arg3 = "hello jni3";
+
+    char* arg4 = "hello jni4";
+
+
+
+    return func1(env);
 
 }
